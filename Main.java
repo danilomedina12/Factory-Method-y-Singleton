4b53aefc78d56bcd854d796ba3e36ed603310a20
Danilo Medina <danilomedina666@gmail.com>
2025-10-01 19:33:09 -0300

    nuevo main

--- Main.java
+++ Main.java
@@ -1,4 +1,3 @@
-import Documento.*;
 import Visualizador.*;
 
 public class Main {
@@ -10,7 +9,20 @@ public class Main {
         VisualizadorODT visualizadorODT = new VisualizadorODT();
         VisualizadorHTML visualizadorHTML = new VisualizadorHTML();
 
+        visualizadorPDF.visualizar();
+        System.out.println("-------------------------------------");
+        visualizadorODT.visualizar();
+        System.out.println("-------------------------------------");
+        visualizadorHTML.visualizar();
 
-        
+        // lo de state
+        State state = State.getInstancia();
+        state.agregarDocumentos("Se visualizó un PDF");
+        state.agregarDocumentos("Se visualizó un ODT");
+        state.agregarDocumentos("Se visualizó un HTML");
+        System.out.println("Historial de acciones:");
+        for(String s : state.obtenerDocumentos()){
+            System.out.println("- " + s);
+        }
     }
 }
